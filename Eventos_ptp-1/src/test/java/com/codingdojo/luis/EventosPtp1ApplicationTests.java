8a43b8b9e674b2c2cae00ccdc703e03a80188c1c
Luis Loyola Cuevas <150284189+luxoi@users.noreply.github.com>
2023-11-21 14:18:32 -0300

    eventos

---
+++ Eventos_ptp-1/src/test/java/com/codingdojo/luis/EventosPtp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.luis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventosPtp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
